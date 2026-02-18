ee4508b8bdfc3107fb3f6e8df39b404d12885387
Gabriel Ledina <gabriel.ledina@student.hepl.be>
2026-02-18 17:10:38 +0100

    added default value for env function

--- tobi/helpers.php
+++ tobi/helpers.php
@@ -11,9 +11,9 @@ if (!function_exists('dd')) {
     }
 }
 if (!function_exists('env')) {
-    function env(string $key): mixed
+    function env(string $key, $default = null): mixed
     {
-        return $_ENV[$key];
+        return $_ENV[$key]??$default;
     }
 }
 
